ffb82a28fca5aef152f2a9120f09bf9afc8436c1
Sergey Zinyaev <zinyaevsi@gmail.com>
2021-09-02 17:15:49 +0500

    *

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from flask_socketio import SocketIO, send
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
 socketio = SocketIO(app, cors_allowed_origins='*')
+users = []
 
 
 @app.route('/', methods=['GET'])
@@ -13,8 +14,14 @@ def hello():
 
 @socketio.on('message')
 def handleMessage(msg):
-    print('Message: ' + msg)
+    print('Message: ' + str(msg))
     send(msg, broadcast=True)
+
+@socketio.on('username', namespace='/private')
+def receive_username(username):
+    users.append({usernam: request.sid})
+    print(users)
+
 
 if __name__ == '__main__':
     socketio.run(app)
