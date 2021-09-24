027664ee3a84cf5398a8c2b92e08a31c2bb3ff36
Sergey Zinyaev <zinyaevsi@gmail.com>
2021-09-24 17:14:54 +0500

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-SECRET_KEY = 'FASF352fFEFfmysecretkvikkvikkvikkvikikvik3%LKMnONLOC5SMhaGgYHbuVRTCUY56JYCvtgv'
-database = "kvik_messenger"
-user = "kvik"
-password = "2262"
-host = "192.168.8.92"
-port = "5432"
