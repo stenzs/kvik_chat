d06c80ec36d3ae1fdccb0e17bf880b8cc1ce6af8
Sergey Zinyaev <zinyaevsi@gmail.com>
2021-10-04 13:57:17 +0500

    *

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def chat_last_messages():
             return jsonify({'message': 'invalid data'}), 422
         subq = Messages.select(fn.MAX(Messages.id).alias('room')).group_by(Messages.room).dicts().where(((Messages.sender_id == user_id) & (Messages.delete != True)) | ((Messages.recipient_id == user_id) & (Messages.delete != True)))
 
-        query = list(Messages.select(Messages.message, Messages.messages_is_read, Messages.sender_id, Messages.time, Rooms.seller_id, Rooms.seller_name, Rooms.seller_photo, Rooms.customer_id, Rooms.customer_name, Rooms.customer_photo, Rooms.product_id, Rooms.product_name, Rooms.product_photo).where(Messages.id.in_(subq)).dicts().order_by(Messages.id.desc()).join(Rooms, on=(Messages.room == Rooms.name)))
+        query = list(Messages.select(Messages.message, Messages.messages_is_read, Messages.sender_id, Messages.time, Rooms.seller_id, Rooms.seller_name, Rooms.seller_photo, Rooms.customer_id, Rooms.customer_name, Rooms.customer_photo, Rooms.product_id, Rooms.product_name, Rooms.product_photo, Rooms.product_price).where(Messages.id.in_(subq)).dicts().order_by(Messages.id.desc()).join(Rooms, on=(Messages.room == Rooms.name)))
         # query = list(Messages.select(Messages.message, Messages.messages_is_read, Messages.sender_id, Messages.time, Rooms.seller_id, Rooms.seller_name, Rooms.seller_photo, Rooms.customer_id, Rooms.customer_name, Rooms.customer_photo, Rooms.product_id, Rooms.product_name, Rooms.product_photo).where(Messages.id.in_(subq)).dicts().order_by(Messages.id.desc()))
 
         return jsonify({'message': 'success', 'data': list(query)}), 200
@@ -136,7 +136,8 @@ def text(message):
     else:
         room = str((message['sender'])['id']) + '&' + str((message['recipient'])['id']) + '&' + str((message['product'])['id'])
     Messages.create(room=room, sender_id=(message['sender'])['id'], recipient_id=(message['recipient'])['id'],
-                    message=message['message'], time=datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+                    message=message['message'], time=datetime.now().strftime({"y": '%Y', "mo": '%m', "d": '%d',
+                                                                              "h": '%H', "mi": '%M'}))
     send(message, to=room)
 
 
