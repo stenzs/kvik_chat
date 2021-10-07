ea9f6c9aba344088ce0b661c6104fca112cabb31
Sergey Zinyaev <zinyaevsi@gmail.com>
2021-10-07 10:45:43 +0500

    *

--- database.py
+++ database.py
@@ -28,15 +28,8 @@ class Rooms(BaseModel):
     id = PrimaryKeyField(column_name='id', primary_key=True, unique=True)
     name = TextField(column_name='name')
     seller_id = IntegerField(column_name='seller_id')
-    seller_name = TextField(column_name='seller_name')
-    seller_photo = TextField(column_name='seller_photo')
     customer_id = IntegerField(column_name='customer_id')
-    customer_name = TextField(column_name='customer_name')
-    customer_photo = TextField(column_name='customer_photo')
     product_id = IntegerField(column_name='product_id')
-    product_name = TextField(column_name='product_name')
-    product_photo = TextField(column_name='product_photo')
-    product_price = IntegerField(column_name='product_price')
 
     class Meta:
         table_name = 'rooms'
