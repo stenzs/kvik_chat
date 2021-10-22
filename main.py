4e6559a5b5a3cea936338471159da0ece5f62a4c
Sergey Zinyaev <zinyaevsi@gmail.com>
2021-10-22 15:28:44 +0500

    *

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ def send_push():
                         'body': 'У вас новое сообщение от ' + user_name + ':\n' + message,
                         'icon': 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Windows_Settings_app_icon.png/1024px-Windows_Settings_app_icon.png',
                         'click_action': 'https://https://google.com/',
-                        'image': 'https://blog.back4app.com/wp-content/uploads/2021/01/Firebase-Cloud-Messaging.png'
+                        'image': 'http://192.168.8.111:6001/images/po/0a/4d/d1/8f/7e…e5b523bd1bf8c5f1738949ab20211019181409232700.webp'
                         },
                     'to': token, 'priority': 'high'}
                 response = requests.post("https://fcm.googleapis.com/fcm/send", headers=headers, data=json.dumps(body))
